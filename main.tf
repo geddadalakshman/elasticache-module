a7b2bca7fca2029022827b030784930f4a114ce6
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-05-04 12:58:27 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
-resource "aws_elasticache_cluster" "main" {
-  cluster_id           = "${var.env}-redis"
-  engine               = var.engine
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  parameter_group_name = "default.redis3.2"
-  engine_version       = var.engine_version
-  db_subnet_group_name = aws_docdb_subnet_group.main.name
-}
+resource "aws_elasticache_cluster" "elasticache" {
+  cluster_id         = "${var.env}-elasticache"
+  engine             = var.engine
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
+  engine_version     = var.engine_version
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.main.name
+  security_group_ids = [aws_security_group.main.id]
 
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-elasticache" }
+  )
+}
 
 resource "aws_security_group" "main" {
   name        = "elasticache-${var.env}"
@@ -36,28 +41,23 @@ resource "aws_security_group" "main" {
   )
 }
 
-resource "aws_docdb_subnet_group" "main" {
-  name       = "${var.env}-redis-subnet_group"
+
+resource "aws_elasticache_subnet_group" "main" {
+  name       = "${var.env}-rds"
   subnet_ids = var.subnet_ids
 
   tags = merge(
     var.tags,
-    {
-      Name = "${var.env}-subnet-group"
-    },
+    { Name = "${var.env}-subnet-group" }
   )
 }
 
 output "redis" {
-  value = aws_elasticache_cluster.main
+  value = aws_elasticache_cluster.elasticache
 }
 
 resource "aws_ssm_parameter" "elasticache_endpoint" {
   name  = "${var.env}.elasticache.endpoint"
   type  = "String"
-  value = aws_elasticache_cluster.main.cache_nodes[0].address
+  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
 }
-
-
-
-
